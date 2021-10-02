8e93d454e081c3956df4d1adf2efc74eeae6578a
yjkim <play1490@naver.com>
2021-10-02 16:22:58 +0900

    for ABB

--- thirdparty/glremote/glremote/glremote.h
+++ thirdparty/glremote/glremote/glremote.h
@@ -14,10 +14,13 @@
 #include <zmq.hpp>
 #include <zmq_addon.hpp>
 
-#define FRAME_BUFFER_ENABLE 1
-#define SEQUENCE_DEDUP_ENABLE 1
-#define COMMAND_DEDUP_ENABLE 1
-#define ASYNC_BUFFER_BINDING 1
+#define FRAME_BUFFER_ENABLE 0
+#define SEQUENCE_DEDUP_ENABLE 0
+#define COMMAND_DEDUP_ENABLE 0
+#define ASYNC_BUFFER_BINDING 0
+#define LATENCY_EXPERIMENTS 1 // LATENCY 측정을 위해 추가 됨
+#define ASYNC_BUFFER_EXPERIMENTS 1 // ABB FRAME 실험
+
 #define CACHE_ENTRY_SIZE MAX_CACHE_ENTRY
 
 #define GL_SET_COMMAND(PTR, FUNCNAME)                                                \
@@ -32,5 +35,9 @@ GLint global_pack_alignment = 4;
 GLint global_unpack_alignment = 4;
 
 int current_sequence_number = 0;
+
+#if ASYNC_BUFFER_EXPERIMENTS
+auto first_frame_time = std::chrono::steady_clock::now();
+#endif
 
 zmq::multipart_t frame_message_buffer;
